023800a88ad45d16f6b6d306daf59e1755ae6fac
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2021-06-14 16:58:25 -0700

    intro to recursion

--- Level3-Module5/src/_01_StringTrimmer/StringTrimmer.java
+++ Level3-Module5/src/_01_StringTrimmer/StringTrimmer.java
@@ -2,18 +2,22 @@ package _01_StringTrimmer;
 
 public class StringTrimmer {
 
-    /*
-     * This method removes the specified number of characters from the right
-     * side of the specified string and returns the trimmed string. 
-     */
-    static public String trimString(String str, int removesLeft) {
-        // 1. If there are no removes left
-            
-            // 2. Return the string
-
-        // 3. Else you need to remove the last letter from the string,
-        //    reduce removesLeft by 1, and then call trimString() 
-
-        return null;
-    }
+	/*
+	 * This method removes the specified number of characters from the right side of
+	 * the specified string and returns the trimmed string.
+	 */
+	static public String trimString(String str, int removesLeft) {
+		// 1. If there are no removes left
+		if (removesLeft <= 0) {
+			// 2. Return the string
+			return str;
+		} else {
+			// 3. Else you need to remove the last letter from the string,
+			// reduce removesLeft by 1, and then call trimString()
+			StringBuilder a = new StringBuilder(str);
+			a.deleteCharAt(a.length());
+			trimString(a.toString(), removesLeft--);
+			return null;
+		}
+	}
 }
